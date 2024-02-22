c00b833ad0a05e639989a9e22676d39266ab939f
RUDYan <112593269+JUSTRUDYan@users.noreply.github.com>
2024-02-22 02:28:43 +0200

    комітик

--- README.md
+++ README.md
@@ -2,15 +2,13 @@
 
 This Bash script provides a command-line interface for creating a new user with RSA keys. It allows users to specify various parameters such as the user name, user folder path, user pass phrase, and RSA key bit length.
 
-### Usage
+### **Usage**
 
 ```bash
-bashCopy code
 ./creuser.sh -n <user_name> -f <user_folder_path> -p <user_pass_phrase> -b <rsa_key_bit_length>
-
 ```
 
-### Options
+### **Options**
 
 - **`n`**: Name of the new user (required).
 - **`f`**: User folder path (required).
@@ -18,12 +16,43 @@ bashCopy code
 - **`b`**: Bit amount of RSA key (optional, default: 4096).
 - **`h`**: Display help message.
 
-### Usage Example
+### **Usage Example**
 
 ```bash
-bashCopy code
 ./creuser.sh -n pupyan -f /home/pupyan -p password1234
-
 ```
 
 Note: The script will prompt for confirmation before proceeding with user creation.
+
+### **Script Description**
+
+The script follows a clean and structured code style. It includes functions for parsing options, checking password phrases, validating RSA key bit lengths, and displaying help messages. Additionally, it prompts users for confirmation and provides information about the user creation process.
+
+### **Output**
+
+Upon successful user creation, the script outputs a connection command for SSH with the newly created user and IP address.
+
+```bash
+Your connection command:
+ssh -i "path_to_rsa_key" pupyan@<IP_ADDRESS>
+```
+
+### **SSH Daemon Restart**
+
+The script also offers an option to restart the SSH daemon after user creation, providing additional flexibility.
+
+### **Running the Script**
+
+To execute the script, use the following command:
+
+```bash
+./creuser.sh -n <user_name> -f <user_folder_path> -p <user_pass_phrase> -b <rsa_key_bit_length>
+```
+
+Ensure that the script has the necessary permissions for execution:
+
+```bash
+chmod +x creuser.sh
+```
+
+If executed directly, the script will create the specified user and RSA keys.
--- main.sh
+++ main.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 readonly DEFAULT_RSA_BIT=4096
+readonly IP_ADDRESS=$(ip addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v '127.0.0.1' | head -n 1)
 
 parse_options() {
     while getopts ":hn:f:p:b:" opt; do
         case $opt in
-        h) help_creuser; exit 0 ;;
-        n) user_name="$OPTARG"; ;;
-        f) user_path="$OPTARG"; ;;
-        p) user_pass_phrase="$OPTARG"; check_pass_phrase "$user_pass_phrase";;
-        b) rsa_bit="$OPTARG"; validate_rsa_bit "$rsa_bit";;
-        \?) echo "$OPTARG is not an option"; exit 1 ;;
+            h) help_creuser; exit 0 ;;
+            n) user_name="$OPTARG"; ;;
+            f) user_path="$OPTARG"; ;;
+            p) user_pass_phrase="$OPTARG"; check_pass_phrase "$user_pass_phrase";;
+            b) rsa_bit="$OPTARG"; validate_rsa_bit "$rsa_bit";;
+            \?) echo "$OPTARG is not an option"; exit 1 ;;
         esac
     done
     shift $OPTIND
@@ -26,12 +27,12 @@ check_pass_phrase() {
 }
 
 validate_rsa_bit() {
-  rsa_bit="$1"
+    rsa_bit="$1"
 
-  if [[ ! $rsa_bit =~ ^[0-9]+$ || $rsa_bit -lt 2048 || $rsa_bit -gt 4096 ]]; then
+    if [[ ! $rsa_bit =~ ^[0-9]+$ || $rsa_bit -lt 2048 || $rsa_bit -gt 4096 ]]; then
         echo "Error: Invalid RSA key bit length. Must be between 2048 and 4096."
         exit 1
-  fi
+    fi
 }
 
 help_creuser() {
@@ -47,25 +48,39 @@ validate_inputs() {
     echo "User creation parameters:"
     echo "  - User Name: $user_name"
     echo "  - User Folder Path: $user_path"
-    echo "  - RSA Key Bit Length: ${rsa_bit:-$DEFAULT_RSA_BIT}"
+    echo "  - RSA Key Bit Length: $rsa_bit"
 
     while true; do
         read -p "Are these parameters correct? (yes/no): " answer
 
         case "$answer" in
-        [Yy]*)
-            break ;;
-        [Nn]*)
-            echo "User creation aborted. Please provide correct parameters."
-            exit 1 ;;
-        *)
-            echo "Invalid input. Please enter 'yes' or 'no'." ;;
+            [Yy]*) break ;;
+            *) echo "User creation aborted. Please provide correct parameters." && exit 1 ;;
+        esac
+    done
+}
+
+output() {
+    echo "Your connection command:"
+    echo "ssh -i \"path_to_rsa_key\" $user_name@$IP_ADDRESS"
+}
+
+ssh_restart() {
+    while true; do
+        read -p "Do you need to restart sshd process? (yes/no): " answer
+
+        case "$answer" in
+            [Yy]*) sudo systemctl restart sshd && break ;;
+            *) echo "sshd restarting canceled." && exit 1 ;;
         esac
     done
 }
 
 creuser() {
     parse_options "$@"
+
+    # Use the DEFAULT_RSA_BIT constant if the user didn't specify a bit length
+    rsa_bit=${rsa_bit:-$DEFAULT_RSA_BIT}
 
     if [[ -z "$user_name" || -z "$user_path" || -z "$user_pass_phrase" ]]; then
         echo "Error: Missing required parameters."
@@ -84,15 +99,16 @@ creuser() {
         mkdir -p "$user_path" || { echo "Failed to create user directory $user_path"; exit 1; }
     fi
 
-    # Use the DEFAULT_RSA_BIT constant if the user didn't specify a bit length
-    rsa_bit=${rsa_bit:-$DEFAULT_RSA_BIT}
-
     ssh-keygen -t rsa -b "$rsa_bit" -f "$user_path/id_rsa" -N "$user_pass_phrase" || { echo "Failed to generate RSA keys with $rsa_bit bits"; exit 1; }
 
     useradd -m -d "$user_path" -s /bin/bash "$user_name" || { echo "Failed to create user $user_name"; exit 1; }
     echo "$user_name:$user_pass_phrase" | chpasswd || { echo "Failed to set password for user $user_name"; exit 1; }
 
     echo "User $user_name created successfully with RSA keys."
+
+    ssh_restart
+
+    output
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
