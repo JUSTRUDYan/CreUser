abef4126811fa10e731e6e9790e4b962187319b7
RUDYan <112593269+JUSTRUDYan@users.noreply.github.com>
2024-02-22 02:09:51 +0200

    комітик

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# CreUser
- Script for easy user creation
+### **Bash Script for User Creation with RSA Keys**
+
+This Bash script provides a command-line interface for creating a new user with RSA keys. It allows users to specify various parameters such as the user name, user folder path, user pass phrase, and RSA key bit length.
+
+### Usage
+
+```bash
+bashCopy code
+./creuser.sh -n <user_name> -f <user_folder_path> -p <user_pass_phrase> -b <rsa_key_bit_length>
+
+```
+
+### Options
+
+- **`n`**: Name of the new user (required).
+- **`f`**: User folder path (required).
+- **`p`**: User pass phrase (required, minimum 10 characters).
+- **`b`**: Bit amount of RSA key (optional, default: 4096).
+- **`h`**: Display help message.
+
+### Usage Example
+
+```bash
+bashCopy code
+./creuser.sh -n pupyan -f /home/pupyan -p password1234
+
+```
+
+Note: The script will prompt for confirmation before proceeding with user creation.
---
+++ main.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+readonly DEFAULT_RSA_BIT=4096
+
+parse_options() {
+    while getopts ":hn:f:p:b:" opt; do
+        case $opt in
+        h) help_creuser; exit 0 ;;
+        n) user_name="$OPTARG"; ;;
+        f) user_path="$OPTARG"; ;;
+        p) user_pass_phrase="$OPTARG"; check_pass_phrase "$user_pass_phrase";;
+        b) rsa_bit="$OPTARG"; validate_rsa_bit "$rsa_bit";;
+        \?) echo "$OPTARG is not an option"; exit 1 ;;
+        esac
+    done
+    shift $OPTIND
+}
+
+check_pass_phrase() {
+    user_pass_phrase="$1"
+
+    if [ ${#user_pass_phrase} -lt 10 ]; then
+        echo "Error: Password phrase is less than 10 characters."
+        exit 1
+    fi
+}
+
+validate_rsa_bit() {
+  rsa_bit="$1"
+
+  if [[ ! $rsa_bit =~ ^[0-9]+$ || $rsa_bit -lt 2048 || $rsa_bit -gt 4096 ]]; then
+        echo "Error: Invalid RSA key bit length. Must be between 2048 and 4096."
+        exit 1
+  fi
+}
+
+help_creuser() {
+    echo "Usage: creuser"
+    echo "  -n Name of new user (required)."
+    echo "  -f User folder path (required)."
+    echo "  -p User pass phrase (required, minimum 10 characters)."
+    echo "  -b Bit amount of RSA key (optional, default: $DEFAULT_RSA_BIT)."
+    echo "  -h Display this help message."
+}
+
+validate_inputs() {
+    echo "User creation parameters:"
+    echo "  - User Name: $user_name"
+    echo "  - User Folder Path: $user_path"
+    echo "  - RSA Key Bit Length: ${rsa_bit:-$DEFAULT_RSA_BIT}"
+
+    while true; do
+        read -p "Are these parameters correct? (yes/no): " answer
+
+        case "$answer" in
+        [Yy]*)
+            break ;;
+        [Nn]*)
+            echo "User creation aborted. Please provide correct parameters."
+            exit 1 ;;
+        *)
+            echo "Invalid input. Please enter 'yes' or 'no'." ;;
+        esac
+    done
+}
+
+creuser() {
+    parse_options "$@"
+
+    if [[ -z "$user_name" || -z "$user_path" || -z "$user_pass_phrase" ]]; then
+        echo "Error: Missing required parameters."
+        help_creuser
+        exit 1
+    fi
+
+    if id "$user_name" &>/dev/null; then
+        echo "User $user_name exists."
+        exit 1
+    fi
+
+    validate_inputs
+
+    if [ ! -d "$user_path" ]; then
+        mkdir -p "$user_path" || { echo "Failed to create user directory $user_path"; exit 1; }
+    fi
+
+    # Use the DEFAULT_RSA_BIT constant if the user didn't specify a bit length
+    rsa_bit=${rsa_bit:-$DEFAULT_RSA_BIT}
+
+    ssh-keygen -t rsa -b "$rsa_bit" -f "$user_path/id_rsa" -N "$user_pass_phrase" || { echo "Failed to generate RSA keys with $rsa_bit bits"; exit 1; }
+
+    useradd -m -d "$user_path" -s /bin/bash "$user_name" || { echo "Failed to create user $user_name"; exit 1; }
+    echo "$user_name:$user_pass_phrase" | chpasswd || { echo "Failed to set password for user $user_name"; exit 1; }
+
+    echo "User $user_name created successfully with RSA keys."
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    creuser "$@"
+fi
